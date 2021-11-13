b5f38ba2c8222341bf4a2c17e856885ab6912129
a45320614 <asd45320614@gmail.com>
2021-11-13 14:57:00 +0800

    更新

--- Python作業/作業二_3.py
+++ Python作業/作業二_3.py
@@ -1,13 +1,18 @@
 import pylab
-n=int()
-def g(t):
-    if t>1:return 1
-    elif t<n:return -1
-        
-
-    
-ts=pylab.linspace(-3,3,100)
-ys=g(ts)
-pylab.plot(ts,ys)
+def h(t):
+    return (1.2732*pylab.sin(2*t))+(0.4244*pylab.sin(6*t))+(0.25465*pylab.sin(10*t))+(0.18189*pylab.sin(14*t))+(0.14147*pylab.sin(18*t))
+def dh2(t,h1):
+    return (h(t+h1)-h(t))/h1
+a,b,n=-pylab.pi,pylab.pi,10000
+h1=(b-a)/(n-1)
+ts=pylab.linspace(a,b,n)
+ys=h(ts)
+pylab.plot(ts,ys,label="h(t)",color="blue")
+ys2=dh2(ts,h1)
+pylab.plot(ts,ys2,label="h'(t)",color="green")
 pylab.grid()
+pylab.xlabel("t")
+pylab.ylabel("Y")
+pylab.title("sawtooth function approximation and derivative")
+pylab.legend(loc=4)
 pylab.show()
